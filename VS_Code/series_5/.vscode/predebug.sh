517e69adbbb6aed96a66def04b6128b81d4a6e05
Sean Donohue <sdonohue331@gmail.com>
2017-06-12 14:54:30 -0600

    Added the cmake tools. Removed symbol databases

--- VS_Code/series_5/.vscode/predebug.sh
+++ VS_Code/series_5/.vscode/predebug.sh
@@ -2,8 +2,8 @@
 
 INSTRUMENT_IP=10.85.4.86
 
-#scp /home/sdonohue/Documents/ASD_GitLab/series_5/instrument/Debug/instrument root@${INSTRUMENT_IP}:/usr/bin
-scp /home/sdonohue/Documents/ASD_GitLab/series_5/instrument/build/instrument root@${INSTRUMENT_IP}:/usr/bin
+scp /home/sdonohue/Documents/ASD_GitLab/series_5/instrument/Debug/instrument root@${INSTRUMENT_IP}:/usr/bin
+#scp /home/sdonohue/Documents/ASD_GitLab/series_5/instrument/build/instrument root@${INSTRUMENT_IP}:/usr/bin
 ssh -T root@${INSTRUMENT_IP}<<EOT
 gdbserver :2345 /usr/bin/instrument
 
